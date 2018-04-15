2a1edb68ee8d3dda8732c4ad017f5f03aa974cef
HuiChang <hchang@thoughtworks.com>
2018-04-15 10:43:45 +0800

    clean what you did

--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -1,23 +1,14 @@
 package com.tw;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.LinkedList;
 
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LibraryTest {
-    private Library library;
-
-    @Before
-    public void setUp() throws Exception {
-        library = new Library();
-    }
-
     @Test
     public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
@@ -37,23 +28,4 @@ public class LibraryTest {
 
     }
 
-    @Test
-    public void testWhenInputFirstReturnInputInfo() {
-        String answer = library.accept(1);
-        assertEquals("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：", answer);
-    }
-
-    @Test
-    public void testWhenInputStudentInfoSuccess() {
-        String info = "常卉, 001, 语文: 80";
-        String saveStudentInfo = library.saveStudentInfo(info);
-        assertEquals("学生xxx的成绩被添加", saveStudentInfo);
-    }
-
-    @Test
-    public void testWhenInputStudentInfoFailed() {
-        String info = "常卉, 001, 语文, 80";
-        String saveStudentInfo = library.saveStudentInfo(info);
-        assertEquals("请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）", saveStudentInfo);
-    }
 }
